516389ff83e7260207512b29c652de30c120ccb1
abhi4267 <135379032+abhi4267@users.noreply.github.com>
2023-06-30 18:19:54 +0530

    Update AuthApi.js

--- client/src/api/AuthApi.js
+++ client/src/api/AuthApi.js
@@ -17,8 +17,8 @@ export async function uploadImage(image){
     const data = new FormData();
     data.append("file",image);
     data.append("upload_preset","eventmapper");
-    data.append("cloud_name","abhihshek")
-    const response = await fetch("https://api.cloudinary.com/v1_1/abhishek/image/upload",{
+    data.append("cloud_name","prashantpks")
+    const response = await fetch("https://api.cloudinary.com/v1_1/prashantpks/image/upload",{
         method: 'POST',
         body: data
     });
